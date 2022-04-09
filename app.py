53854b6805aae0f35a4af296634af5f7387f8f5b
lenargasimov <lenargasimov@duck.com>
2022-04-09 09:47:01 +0300

    feat: added emailing data statistics to users

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db_password = os.environ.get('DB_PASSWORD')
-app.config['SQLALCHEMY_DATABASE_URI'] = db_password
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 
 
@@ -33,11 +33,14 @@ def success():
     if request.method == "POST":
         email = request.form["email_name"]
         height = request.form["height_name"]
-        send_email(email, height)
         if db.session.query(Data).filter(Data.email_ == email).count() == 0:
             data = Data(email, height)
             db.session.add(data)
             db.session.commit()
+            average_height = db.session.query(func.avg(Data.height_)).scalar()
+            average_height = round(average_height, 1)
+            count = db.session.query(Data.height_).count()
+            send_email(email, height, average_height, count)
             return render_template("success.html")
     return render_template("index.html", 
     text = "Seems like we've got something from that email address already!")
